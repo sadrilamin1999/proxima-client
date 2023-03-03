fc15d0dba22d149a9a63b79e3332d8d83b833275
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-04 02:10:02 +0600

    logout added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+
+  const { logout } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <div className="navbar container mx-auto h-20 flex items-center justify-between border-b border-stone-400/40">
       <Link
@@ -10,12 +19,28 @@ const Navbar = () => {
         Proxima
       </Link>
       <nav className="flex gap-5">
-        <Link to="login" className="hover:text-sky-400 duration-300">
-          Login
-        </Link>
-        <Link to="signup" className="hover:text-sky-400 duration-300">
-          Signup
-        </Link>
+        {!user && (
+          <div className="flex gap-5">
+            <Link to="login" className="hover:text-sky-400 duration-300">
+              Login
+            </Link>
+            <Link to="signup" className="hover:text-sky-400 duration-300">
+              Signup
+            </Link>
+          </div>
+        )}
+        {user && (
+          <div className="flex gap-5 items-center">
+            <span>{user.email}</span>
+            <button
+              onClick={handleLogout}
+              type="submit"
+              className="bg-rose-500 text-white py-3 px-5 rounded-lg hover:bg-sky-50 hover:text-slate-900 duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
     </div>
   );
