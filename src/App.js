03add7abe331b5925c99495f7ffcfd27df65b0a3
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-03 12:11:10 +0600

    auth context and login signup added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="app bg-stone-200 text-gray-600 min-h-screen">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </div>
